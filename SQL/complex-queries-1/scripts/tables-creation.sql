d56c8ea1994bddb9444844adfcc55cd1d37a9752
A-Cobra <conradbravina@gmail.com>
2024-04-10 08:21:46 -0500

    Created the database using docker and executed some SQL files
    The password and other secrets must not be included in tthe dokcer files but it is just for testing purposes

--- SQL/complex-queries-1/scripts/tables-creation.sql
+++ SQL/complex-queries-1/scripts/tables-creation.sql
@@ -1,5 +1,27 @@
 USE `aizen_courses`;
 
+-- Create author table
+CREATE TABLE author (
+    author_id INT PRIMARY KEY,
+    author_last_name VARCHAR(255),
+    author_name VARCHAR(255),
+    author_bio TEXT,
+    is_active BOOLEAN
+);
+
+-- Create category table
+CREATE TABLE category (
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(255)
+);
+
+-- Create department table
+CREATE TABLE department (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(255),
+    foundation_date DATE
+);
+
 -- Create course table
 CREATE TABLE course (
     course_id INT PRIMARY KEY,
@@ -21,18 +43,16 @@ CREATE TABLE student (
     student_id INT PRIMARY KEY,
     student_last_name VARCHAR(255),
     student_name VARCHAR(255),
-    email VARCHAR(255),
-    subscription_id INT,
-    FOREIGN KEY (subscription_id) REFERENCES student_subscription(subscription_id)
+    email VARCHAR(255)
 );
 
--- Create author table
-CREATE TABLE author (
-    author_id INT PRIMARY KEY,
-    author_last_name VARCHAR(255),
-    author_name VARCHAR(255),
-    author_bio TEXT,
-    is_active BOOLEAN
+-- Create student_subscription table
+CREATE TABLE student_subscription (
+    subscription_id INT PRIMARY KEY,
+    student_id INT UNIQUE,
+    subscription_date DATE,
+    expiration_date DATE,
+    FOREIGN KEY (student_id) REFERENCES student(student_id)
 );
 
 -- Create course_student table
@@ -42,27 +62,5 @@ CREATE TABLE course_student (
     enrollment_date DATE,
     is_completed BOOLEAN,
     FOREIGN KEY (course_id) REFERENCES course(course_id),
-    FOREIGN KEY (student_id) REFERENCES student(student_id)
-);
-
--- Create department table
-CREATE TABLE department (
-    department_id INT PRIMARY KEY,
-    department_name VARCHAR(255),
-    foundation_date DATE
-);
-
--- Create category table
-CREATE TABLE category (
-    category_id INT PRIMARY KEY,
-    category_name VARCHAR(255)
-);
-
--- Create student_subscription table
-CREATE TABLE student_subscription (
-    subscription_id INT PRIMARY KEY,
-    student_id INT UNIQUE,
-    subscription_date DATE,
-    expiration_date DATE,
     FOREIGN KEY (student_id) REFERENCES student(student_id)
 );
