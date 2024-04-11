e57c4802a254fbd74058e78280fab027715274be
A-Cobra <conradbravina@gmail.com>
2024-04-11 18:01:09 -0500

    Adding some sql files for the datase creation

---
+++ SQL/complex-queries-1/scripts/inserting-data/department.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (department_id, department_name, foundation_date) VALUES
+  (1, 'Computer Science', '1998-06-24'),
+  (2, 'Economy', '1978-03-16'),
+  (3, 'Personal Debelopment', '2015-02-03'),
+  (4, 'Biology', '1996-05-17'),
+  (5, 'Business Administration', '2008-12-15'),
+  (6, 'Mechanical Engineering', '1995-01-16'),
+  (7, 'Humanities', '2003-12-09');
