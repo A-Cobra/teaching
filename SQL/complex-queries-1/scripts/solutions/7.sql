8cdd4bbd9a7369549d4d1388782ad07907de30ad
A-Cobra <conradbravina@gmail.com>
2024-05-12 14:25:33 -0500

    Rectifying query 7

--- SQL/complex-queries-1/scripts/solutions/7.sql
+++ SQL/complex-queries-1/scripts/solutions/7.sql
@@ -8,10 +8,15 @@ WITH course_histogram AS (
   JOIN course c 
   ON c.course_id  = cs.course_id
   GROUP BY c.course_id, c.price, c.course_id
+),
+author_partial_revenue_histogram AS (
+  SELECT a.author_id, a.author_name, a.author_last_name , ch.price, ch.course_count, ch.price * ch.course_count partial_revenue
+  FROM author a
+  JOIN course_histogram ch
+  ON ch.author_id = a.author_id
 )
-SELECT a.author_id, a.author_name, a.author_last_name , ch.price, ch.course_count, ch.price * ch.course_count total_revenue
-FROM author a
-JOIN course_histogram ch
-ON ch.author_id = a.author_id
+SELECT aprh.author_id, aprh.author_name, aprh.author_last_name, SUM(aprh.partial_revenue) total_revenue
+FROM author_partial_revenue_histogram aprh
+GROUP BY aprh.author_id, aprh.author_name, aprh.author_last_name, aprh.partial_revenue
 ORDER BY total_revenue DESC
 LIMIT 1;
